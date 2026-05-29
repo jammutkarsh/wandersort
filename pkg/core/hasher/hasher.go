d54cb80fe9376ec375bcf9d0034b7731233d2c5e
Utkarsh Chourasia <5.utkarshchourasia@gmail.com>
2026-05-29 22:35:43 +0530

    feat: Integrate utility for absolute path resolution and remove deprecated path resolver

--- pkg/core/hasher/hasher.go
+++ pkg/core/hasher/hasher.go
@@ -22,6 +22,7 @@ import (
 	"github.com/jammutkarsh/wandersort/pkg/db"
 	"github.com/jammutkarsh/wandersort/pkg/logger"
 	"github.com/jammutkarsh/wandersort/pkg/status"
+	"github.com/jammutkarsh/wandersort/pkg/util"
 	"lukechampine.com/blake3"
 )
 
@@ -39,6 +40,7 @@ type Hasher struct {
 	log       logger.Logger
 	scorer    *Scorer
 	statusMgr *status.StatusManager
+	util      *util.Util
 }
 
 type hashSessionTracker struct {
@@ -60,6 +62,7 @@ func NewHasher(appCtx context.Context, db *db.DB, log logger.Logger, sm *status.
 		log:       log,
 		scorer:    NewScorer(db, log),
 		statusMgr: sm,
+		util:      util.NewUtil(),
 	}
 }
 
@@ -349,7 +352,7 @@ func (h *Hasher) HashPath(ctx context.Context, sessionID uuid.UUID, path string,
 
 			absPath := filePath
 			if pathType != scanner.PathTypeAbsolute {
-				absPath = scanner.ResolveAbsolute(filePath, sourceRoot)
+				absPath = h.util.MakeAbsolute(filePath, sourceRoot)
 			}
 
 			batch = append(batch, FileRecord{ID: id, AbsPath: absPath})
